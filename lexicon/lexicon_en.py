7c11d79aa392dd7e9b7d9456fa38aabd760c7773
NDNC <nedanaec@gmail.com>
2025-08-09 22:10:28 +0300

    codex ~ Add last_interest_at to Course model

--- lexicon/lexicon_en.py
+++ lexicon/lexicon_en.py
@@ -185,12 +185,12 @@ LEXICON = {
                         "<code>{course_name}, {name}, tx_id: {tx_id}</code>",
 
     # Interest accrual notifications
-    "interest_savings": "{course_name}\n💹 {amount} 🪙 interest added to your savings.",
-    "interest_loan": "{course_name}\n💸️ {amount} 🪙 interest added to your loan in.",
+    "interest_savings": "{course_name}\n💹 {amount} interest added to your savings.",
+    "interest_loan": "{course_name}\n💸️ {amount} interest added to your loan in.",
     "interest_admin_stats": (
         "Weekly interest applied for {course_name}:\n"
-        "💹 Savings: {s_count} participants, total {s_total} 🪙\n"
-        "💸️ Loan: {l_count} participants, total {l_total} 🪙"
+        "💹 Savings: {s_count} participants, total {s_total}\n"
+        "💸️ Loan: {l_count} participants, total {l_total}"
     ),
 
     # region --- Withdraw and Deposit Flows ---
