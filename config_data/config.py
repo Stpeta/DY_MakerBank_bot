6fde33adb90c9126ee4913d68d01c99ebac635cd
NDNC <nedanaec@gmail.com>
2025-08-11 00:22:44 +0300

    ~ docker tuning

--- config_data/config.py
+++ config_data/config.py
@@ -27,7 +27,7 @@ def load_config(path: str | None = None) -> Config:
             admin_ids=list(map(int, env.list('ADMIN_IDS')))
         ),
         db=DB(
-            url="sqlite+aiosqlite:///db/makerbank.db"
+            url="sqlite+aiosqlite:////db/makerbank.db"
         ),
         SERVICE_ACCOUNT_FILE="service_account.json",
         POSTMARK_API_TOKEN = env('POSTMARK_API_TOKEN')
